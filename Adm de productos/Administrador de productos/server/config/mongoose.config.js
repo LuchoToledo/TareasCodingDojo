c9febb682d486d22444e5490e49262bf674fa37f
LuchoToledo <118233128+LuchoToledo@users.noreply.github.com>
2023-04-16 22:52:07 -0400

    Admistrador

--- Adm de productos/Administrador de productos/server/config/mongoose.config.js
+++ Adm de productos/Administrador de productos/server/config/mongoose.config.js
@@ -4,4 +4,4 @@ mongoose.connect('mongodb://127.0.0.1:27017/productos_db',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
     }).then(()=> console.log('Conexion Establecida'))
-        .catch(err => console.log('Conexion Fallida', err));
+        .catch(err => console.log('Conexion Fallida', err)); 
